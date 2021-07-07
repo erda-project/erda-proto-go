eaf37e2d6aede47c412616d6add3c1905b567ced
innerpeacez <innerpeace.zhai@gmail.com>
2021-07-07 11:44:19 +0800

    exception api

--- msp/apm/exception/pb/exception.validator.pb.go
+++ msp/apm/exception/pb/exception.validator.pb.go
@@ -9,6 +9,7 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
+	_ "google.golang.org/protobuf/types/descriptorpb"
 	_ "google.golang.org/protobuf/types/known/structpb"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
