eb046d716dc3e53927a6796c63805f8da2535ac6
innerpeacez <innerpeace.zhai@gmail.com>
2021-07-28 17:54:00 +0800

    fix msp tenant

--- msp/apm/trace/pb/trace.validator.pb.go
+++ msp/apm/trace/pb/trace.validator.pb.go
@@ -5,12 +5,12 @@ package pb
 
 import (
 	fmt "fmt"
-	math "math"
 	proto "github.com/golang/protobuf/proto"
 	_ "github.com/mwitkow/go-proto-validators"
+	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "google.golang.org/protobuf/types/descriptorpb"
-	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
