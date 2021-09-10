d6ec7f1bbace8e1ded79221e5b15f6f141063b1a
recallsong <songruiguo@qq.com>
2021-09-10 13:48:18 +0800

    add checker openapi

--- msp/apm/exception/pb/exception.http.pb.go
+++ msp/apm/exception/pb/exception.http.pb.go
@@ -113,11 +113,11 @@ func RegisterExceptionServiceHandler(r http.Router, srv ExceptionServiceHandler,
 					}
 				}
 				params := r.URL.Query()
-				if vals := params["scopeId"]; len(vals) > 0 {
-					in.ScopeID = vals[0]
-				}
 				if vals := params["exceptionId"]; len(vals) > 0 {
 					in.ExceptionID = vals[0]
+				}
+				if vals := params["scopeId"]; len(vals) > 0 {
+					in.ScopeID = vals[0]
 				}
 				out, err := handler(ctx, &in)
 				if err != nil {
